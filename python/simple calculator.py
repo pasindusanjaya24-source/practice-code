6079806eba66beaea2ae513ccc2878b36d5fbfeb
pasindu <pasindusanjaya24@gmail.com>
2026-05-23 22:21:50 +0530

    add new option in to code

--- python/simple calculator.py
+++ python/simple calculator.py
@@ -9,7 +9,7 @@ def divide(a,b):
         return a/b 
     except ZeroDivisionError as e:
         print(e)
-        return
+        return None
 def power(a,b):
     return a**b
 def remainder(a,b):
@@ -17,12 +17,59 @@ def remainder(a,b):
         return a%b
     except ZeroDivisionError as e:
         print(e)
-        return
-                        
+        return None
+
 
 #-------------------------------------
 #TODO: Write the select_op(choice) function here
 #This function sould cover Task 1 (Section 2) and Task 3
+def select_op(choice):
+    if '#' in choice:
+        return -1
+    if '$' in choice:
+        return 0
+    if choice not in ['+', '-', '*', '/', '^', '%']:
+        print("Unrecognized operation")
+        return 0
+#get 1st input for calculate
+    while True:
+        num1_str=input("Enter first number: ").strip()
+        if '#' in num1_str:
+            return -1 
+        if '$' in num1_str:
+            return 0
+        try:
+            num1=float(num1_str)
+            break
+        except ValueError:
+            print("Not a valid number,please enter again")  
+#get 2nd input for calculate
+    while True:
+        num2_str=input("Enter second number: ").strip()
+        if '#' in num2_str:
+            return -1 
+        if '$' in num2_str:
+            return 0
+        try:
+            num2=float(num2_str)
+            break
+        except ValueError:
+            print("Not a valid number,please enter again")  
+        
+    if choice == "+":
+        result=add(num1,num2)
+    elif choice =="-":
+        result=substract(num1,num2)
+    elif choice == "*":
+        result=multiply(num1,num2)
+    elif choice == "/":
+        result=divide(num1,num2)
+    elif choice == "^":
+        result=power(num1,num2)
+    elif choice == "%":
+        result=remainder(num1,num2)
+    print(f"{num1} {choice} {num2} = {result}")
+    return 0                        
 
 #End the select_op(choice) function here
 #-------------------------------------
