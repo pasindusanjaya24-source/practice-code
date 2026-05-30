8e97374a9eddf33db10704fe36fcf51a24d0b18f
pasindu <pasindusanjaya24@gmail.com>
2026-05-30 14:18:42 +0530

    create a small logging page using otp generate system

--- java/my_fistjava/src/practicejava/Main.java
+++ java/my_fistjava/src/practicejava/Main.java
@@ -2,20 +2,65 @@ package practicejava;
 import java.util.Scanner;
 public class Main{
 	public static void main(String []args) {
+
 		Scanner x =new Scanner(System.in);
+		
 		int totalBonus = 50000;
-		System.out.print("Enter a Number of Employee: ");
+		System.out.println("Enter a Number of Employee: ");
 		int totalEmployees=x.nextInt();
+		x.nextLine();
 		shareBonus(totalBonus,totalEmployees);
-		x.close();
+		
+		//logging system
+		
+		System.out.println("\t======= Alert! ========");
+		System.out.println("if you get more details,you will log in to system.please use the otp code for logging!");
+		
+		int m=generateotp();
+		checkotp(m,x);
+		
+		
+		
+		//x.close();
 	}
 	public static void shareBonus(int bonus, int employees) {
 		try {
 			int av=bonus/employees;
-			System.out.print("amount for per employee: "+av);
+			System.out.println("amount for per employee: "+av);
 		}
 		catch(ArithmeticException e){
-			System.out.print("Error: Number of employees cannot be zero!");
+			System.out.println("Error: Number of employees cannot be zero!");
 		}
 	}
+	public static int generateotp() {
+		int otp_num=(int)(Math.random()*(999-100+1))+100;
+		return otp_num;
+		
+	}
+	public static void checkotp(int m,Scanner x) {
+		boolean f=false;
+		
+		String otp_gen=String.valueOf(m);
+        System.out.println("\nMasseage Alert!\nOTP Code:"+m+"\n======= !Warning! ====== :\nplease do not give that code.");
+		
+		int attempt=0;
+		
+		while(attempt<3){
+			System.out.println("Enter your otp code: ");
+			String otpcode=x.nextLine();
+			if(otpcode.equals(otp_gen)) {
+				System.out.println("Logging sucessfully!");
+				f=true;
+				break;
+			}
+			attempt++;
+		}
+		
+		if(!f) {
+			System.out.println("Wrong OTP!  you do not have more Attempts!.Try again!");
+			
+			checkotp( generateotp(),x);
+		}
+	}
+	
 }
