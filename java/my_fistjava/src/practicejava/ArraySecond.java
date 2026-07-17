55717a0c0591d856b4350cf999433e9abe1488aa
pasindu <pasindusanjaya24@gmail.com>
2026-07-17 22:57:26 +0530

    refactor: optimize array tasks with dynamic sizing and nested loops

--- java/my_fistjava/src/practicejava/ArraySecond.java
+++ java/my_fistjava/src/practicejava/ArraySecond.java
@@ -4,19 +4,52 @@ public class ArraySecond{
 		Scanner input=new Scanner(System.in);
 		int x;
 		int [] array=new int[5];
-		int [] reArray=new int[5];
+		
+		int evenCount=0;
+		int oddCount=0;
 		
 		for(int i=0;i<array.length;i++){
 			System.out.print("Enter a ["+i+"] element: ");
 			x=input.nextInt();
 			array[i]=x;
 			}
+			for(int i=0;i<array.length;i++){
+			if((array[i]%2)==0){
+				evenCount++;
+			}
+			if((array[i]%2)!=0){
+				oddCount++;
+			}}
+			
+			int [] evenArray=new int[evenCount];
+			int [] oddArray=new int[oddCount];
+			int t=0;
+			int u=0;
+			
 		for(int i=0;i<array.length;i++){
-			reArray[i]=array[(array.length-1)-i]	;
+			if (array[i] % 2 == 0) {
+                evenArray[t] = array[i];
+			    t++;
+			}
+			else{
+				oddArray[u]=array[i];
+				u++;
+			}
 		}
-		System.out.print("Reversed Array: ");
-		for(int i=0;i<array.length;i++){
-			System.out.print(reArray[i]+" ");
+		
+		System.out.print("Even array: ");
+		for(int i=0;i<evenArray.length;i++){
+			if(evenArray[i]!=0){
+				System.out.print(evenArray[i]);
+			}
+			
+		}
+		System.out.println();
+		System.out.print("odd array: ");
+		for(int i=0;i<oddArray.length;i++){
+			if(oddArray[i]!=0){
+				System.out.print(oddArray[i]);
+			}
 		}
 		
 	}
