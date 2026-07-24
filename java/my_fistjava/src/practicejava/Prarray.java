07645e43ce16d159a3e48b3226ae72d1c65c7417
pasindu <pasindusanjaya24@gmail.com>
2026-07-24 16:23:56 +0530

    practice linear search

--- java/my_fistjava/src/practicejava/Prarray.java
+++ java/my_fistjava/src/practicejava/Prarray.java
@@ -1,5 +1,30 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Prarray{
+	
+	public static void linearSearch(int [] arr1,int num){
+		int size=arr1.length;
+		int indexc=-1;
+		try{
+
+			for(int i=0;i<arr1.length;i++){
+				if(arr1[i]==num){
+					indexc=i;
+					break;
+				}
+			
+			}
+			if(indexc!= -1){
+				System.out.println("element found!");
+			}else{
+				System.out.println("element not founded");
+			}
+		}	
+		catch (Exception e) { 
+            System.out.println(e);
+        }
+	}
+
 	public static void main(String []args){
 		Scanner x=new Scanner(System.in);
 		int arr[]={1,2,3,4,5};
@@ -35,6 +60,47 @@ public class Prarray{
 		for(int i=0;i<rearr.length;i++){
 			System.out.print(rearr[i]+" ");
 		}
+		
+		
+		//odd and even array
+		int []array1={1,2,3,4,5};
+	
+		int evenC=0;
+		int oddC=0;
+		
+		for(int i=0;i<array1.length;i++){
+			if((array1[i]%2)==0){
+				evenC++;
+			}else{
+				oddC++;
+			}
+		}
+		int []oddArray=new int[oddC];
+		int []evenArray=new int[evenC];
+		int evenI=0;
+		int oddI=0;
+		for(int i=0;i<array1.length;i++){
+			if((array1[i]%2)==0){
+				evenArray[evenI]=array1[i];
+				evenI++;
+			}else{
+				oddArray[oddI]=array1[i];
+				oddI++;
+			}
+			
+		}
+		System.out.println(Arrays.toString(array1));
+		System.out.println(Arrays.toString(evenArray));
+		System.out.println(Arrays.toString(oddArray));
+		
+		
+		
+		// linear serach
+		int [] arr1={1,2,3,4,5};
+		System.out.print("Enter a element: ");
+		int elemnetC=x.nextInt();
+		linearSearch(arr1,elemnetC);
+		
 		
 	}
 
