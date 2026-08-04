f71ada770a70bb095fe0fc5f444750fb60bc5826
pasindu <pasindusanjaya24@gmail.com>
2026-08-05 00:28:10 +0530

    practice selection sorting and html form

--- java/my_fistjava/src/practicejava/sort.java
+++ java/my_fistjava/src/practicejava/sort.java
@@ -1,4 +1,5 @@
 public class sort {
+	//bubble Sorting
 	public static void bubbleSort(int[]arr){
 		int temp=0;
 		int n=arr.length;
@@ -12,13 +13,31 @@ public class sort {
 			}
 		}
 	}
+	
+	//selection sorting
+	public static void selection(int []arr){
+		int size=arr.length;
+		for(int i=0;i<size;i++){
+			int min_index=i;
+			for(int j=i+1;j<size;j++){
+				if(arr[min_index]>arr[j]){  //inner loop use for find the min index in unsorted array 
+					min_index=j;
+				}
+			}
+			int temp=arr[min_index];
+			arr[min_index]=arr[i];
+			arr[i]=temp;
+		}
+	}
+	
 	public static void main(String []args){
 		int []arr={5,1,12,-5,16};
 		System.out.println("Before the sorting: ");
 		for(int i=0;i<arr.length;i++){
 			System.out.print(arr[i]+" ");
 		}
-		bubbleSort(arr);
+		//bubbleSort(arr);
+		selection(arr);         
 		System.out.println("\nAfter the sorting: ");
 		for(int i=0;i<arr.length;i++){
 			System.out.print(arr[i]+" ");
